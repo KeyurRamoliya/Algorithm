25ddf6700678d1aca7fce52fe23ec7ba97d816f2
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-10-04 20:52:44 +0530

    Fibonacci Added

---
+++ Algorithm/DynamicProgramming/Fibonacci/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithm.DynamicProgramming.Fibonacci
+{
+    public class Fibonacci
+    {
+        public static int Main(int limit)
+        {
+            int result = 0, a = 0, b = 1;
+            for (var i = 0; i <= limit; i++)
+            {
+                var temp = a + b;
+                result = temp;
+                a = b;
+                b = temp;
+            }
+            return result;
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/Fibonacci/FibonacciTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.Fibonacci
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibonacciTest()
+        {
+            int expected = Algorithm.DynamicProgramming.Fibonacci.Fibonacci.Main(10);
+            int actual = 144;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
