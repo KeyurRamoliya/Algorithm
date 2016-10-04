7ac191fbf226123eb18735540c70a7a7b8206510
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-10-04 20:54:55 +0530

    Catalan Number Added

---
+++ Algorithm/DynamicProgramming/CatalanNumber/CatalanNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithm.DynamicProgramming.CatalanNumber
+{
+    public class CatalanNumber
+    {
+        public static int Main(int number)
+        {
+            int result = 0;
+            if (number <= 1) return 1;
+            for (int i = 0; i < number; i++)
+            {
+                result += Main(i)*Main(number - i - 1);
+            }
+            return result;
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/CatalanNumber/CatalanNumberTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.CatalanNumber
+{
+    [TestClass]
+    public class CatalanNumberTests
+    {
+        [TestMethod]
+        public void CatalanNumberTest()
+        {
+            int expected = Algorithm.DynamicProgramming.CatalanNumber.CatalanNumber.Main(10);
+            int actual = 16796;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
