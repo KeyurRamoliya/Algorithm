a8169c74e7698041fe90521dc1feafe2c4ebe2f5
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-10-04 20:56:45 +0530

    Bubble sort algorithm Added

---
+++ Algorithm/Sorting/BubbleSort/BubbleSort.cs
@@ -0,0 +1,25 @@
+﻿namespace Algorithm.Sorting.BubbleSort
+{
+    public class BubbleSort
+    {
+        public static void SortBubble(int[] input)
+        {
+            for (var i = input.Length - 1; i >= 0; i--)
+            {
+                for (var j = input.Length - 1 - 1; j >= 0; j--)
+                {
+                    if (input[j] <= input[j + 1]) continue;
+                    var temp = input[j + 1];
+                    input[j + 1] = input[j];
+                    input[j] = temp;
+                }
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortBubble(input);
+            return input;
+        }
+    }
+}
