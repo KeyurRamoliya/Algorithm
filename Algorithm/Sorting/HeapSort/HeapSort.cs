59a31dcd7435d72d85e6c7daa737b3124b3b102b
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-10-07 22:04:34 +0530

    Heap sort algorithm added

---
+++ Algorithm/Sorting/HeapSort/HeapSort.cs
@@ -0,0 +1,47 @@
+﻿namespace Algorithm.Sorting.HeapSort
+{
+    public class HeapSort
+    {
+        public static void Heapify(int[] input, int n, int i)
+        {
+            int largest = i;
+            int l = i + 1;
+            int r = i + 2;
+
+            if (l < n && input[l] > input[largest])
+                largest = l;
+
+            if (r < n && input[r] > input[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                var temp = input[i];
+                input[i] = input[largest];
+                input[largest] = temp;
+                Heapify(input, n, largest);
+            }
+        }
+
+        public static void SortHeap(int[] input, int n)
+        {
+            for (var i = n - 1; i >= 0; i--)
+            {
+                Heapify(input, n, i);
+            }
+            for (int j = n - 1; j >= 0; j--)
+            {
+                var temp = input[0];
+                input[0] = input[j];
+                input[j] = temp;
+                Heapify(input, j, 0);
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortHeap(input, input.Length);
+            return input;
+        }
+    }
+}
---
+++ AlgorithmTests/Sorting/HeapSort/HeapSortTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.Sorting.HeapSort
+{
+    [TestClass]
+    public class HeapSortTests
+    {
+        [TestMethod]
+        public void HeapSortTestOne()
+        {
+            int[] expected = Algorithm.Sorting.HeapSort.HeapSort.Main(new[] { 1, 5, 2, 7, 8, 4 });
+            int[] actual = { 1, 2, 4, 5, 7, 8 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HeapSortTestTwo()
+        {
+            int[] expected = Algorithm.Sorting.HeapSort.HeapSort.Main(new[] { 100, 7, 290, 07, 18, 74 });
+            int[] actual = { 7, 07, 18, 74, 100, 290 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HeapSortTestThree()
+        {
+            int[] expected = Algorithm.Sorting.HeapSort.HeapSort.Main(new[] { -1, 52, -8964, 547, -191, 0, 85976 });
+            int[] actual = { -8964, -191, -1, 0, 52, 547, 85976 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
