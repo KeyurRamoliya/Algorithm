c0a0038a0f7d1e984d5e3098a4492b5de23a86cb
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-10-07 22:02:15 +0530

    Cycle sort algorithm added

---
+++ Algorithm/Sorting/CycleSort/CycleSort.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Algorithm.Sorting.CycleSort
+{
+    public class CycleSort
+    {
+        public static void SortCycle(int[] input)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                var item = input[i];
+                var position = i;
+                do
+                {
+                    var k = input.Where((t, j) => position != j && t < item).Count();
+                    if (position == k) continue;
+                    while (position != k && item == input[k])
+                    {
+                        k++;
+                    }
+                    var temp = input[k];
+                    input[k] = item;
+                    item = temp;
+                    position = k;
+                } while (position != i);
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortCycle(input);
+            return input;
+        }
+    }
+}
