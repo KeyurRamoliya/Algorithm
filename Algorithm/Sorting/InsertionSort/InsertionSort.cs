d5bcd58092ef549fce4a55bf2b298e945a71f17d
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-10-08 19:39:45 +0530

    Insertion sort algorithm added

---
+++ Algorithm/Sorting/InsertionSort/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithm.Sorting.InsertionSort
+{
+    public class InsertionSort
+    {
+        public static void SortInsertion(int[] input, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int x = input[i];
+                int j = i - 1;
+                while (j >= 0 && input[j] > x)
+                {
+                    input[j + 1] = input[j];
+                    j = j - 1;
+                }
+                input[j + 1] = x;
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortInsertion(input, input.Length);
+            return input;
+        }
+    }
+}
