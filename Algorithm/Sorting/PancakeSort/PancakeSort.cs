4bc719eacf14570d632de65cea471bc35118bd5e
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-14 20:55:07 +0530

    Pancake sort algorithm added

---
+++ Algorithm/Sorting/PancakeSort/PancakeSort.cs
@@ -0,0 +1,45 @@
+﻿namespace Algorithm.Sorting.PancakeSort
+{
+    public class PancakeSort
+    {
+        private static void SortPancake(int[] input, int n)
+        {
+            for (var bottom = n - 1; bottom > 0; bottom--)
+            {
+                var index = bottom;
+                var maxIndex = input[bottom];
+                int i;
+                for (i = bottom - 1; i >= 0; i--)
+                {
+                    if (input[i] > maxIndex)
+                    {
+                        maxIndex = input[i];
+                        index = i;
+                    }
+                }
+                if (index == bottom) continue;
+                var temp = new int[n];
+                var j = 0;
+                for (i = bottom; i > index; i--,j++)
+                {
+                    temp[j] = input[i];
+                }
+                for (i = 0; i < index; i++, j++)
+                {
+                    temp[j] = input[i];
+                }
+                if (temp.Length > j) temp[j] = input[index];
+                for (i = 0; i <= bottom; i++)
+                {
+                    input[i] = temp[i];
+                }
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortPancake(input, input.Length);
+            return input;
+        }
+    }
+}
---
+++ AlgorithmTests/Sorting/PancakeSort/PancakeSortTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.Sorting.PancakeSort
+{
+    [TestClass]
+    public class PancakeSortTests
+    {
+        [TestMethod]
+        public void PancakeSortTestOne()
+        {
+            int[] expected = Algorithm.Sorting.PancakeSort.PancakeSort.Main(new[] { 1, 5, 2, 7, 8, 4 });
+            int[] actual = { 1, 2, 4, 5, 7, 8 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PancakeSortTestTwo()
+        {
+            int[] expected = Algorithm.Sorting.PancakeSort.PancakeSort.Main(new[] { 100, 7, 290, 07, 18, 74 });
+            int[] actual = { 7, 07, 18, 74, 100, 290 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PancakeSortTestThree()
+        {
+            int[] expected = Algorithm.Sorting.PancakeSort.PancakeSort.Main(new[] { -1, 52, -8964, 547, -191, 0, 85976 });
+            int[] actual = { -8964, -191, -1, 0, 52, 547, 85976 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
