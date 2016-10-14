a10a8f32a28d1247a6d9ec5f9d067a91e9c845ca
Keyur Ramoliya <Keyur Ramoliya>
2016-10-14 20:48:40 +0530

    Odd-Even sort algorithm added

---
+++ AlgorithmTests/Sorting/OddEvenSort/OddEvenSortTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.Sorting.OddEvenSort
+{
+    [TestClass]
+    public class OddEvenSortTests
+    {
+        [TestMethod]
+        public void OddEvenSortTestOne()
+        {
+            int[] expected = Algorithm.Sorting.OddEvenSort.OddEvenSort.Main(new[] { 1, 5, 2, 7, 8, 4 });
+            int[] actual = { 1, 2, 4, 5, 7, 8 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddEvenSortTestTwo()
+        {
+            int[] expected = Algorithm.Sorting.OddEvenSort.OddEvenSort.Main(new[] { 100, 7, 290, 07, 18, 74 });
+            int[] actual = { 7, 07, 18, 74, 100, 290 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddEvenSortTestThree()
+        {
+            int[] expected = Algorithm.Sorting.OddEvenSort.OddEvenSort.Main(new[] { -1, 52, -8964, 547, -191, 0, 85976 });
+            int[] actual = { -8964, -191, -1, 0, 52, 547, 85976 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
