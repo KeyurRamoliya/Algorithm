e2896a90e8592d8d4c79979f3d8a4a06451ce88f
Keyur Ramoliya <Keyur Ramoliya>
2016-10-14 20:39:53 +0530

    Merge sort algorithm added

---
+++ Algorithm/Sorting/MergeSort/MergeSort.cs
@@ -0,0 +1,75 @@
+﻿namespace Algorithm.Sorting.MergeSort
+{
+    public class MergeSort
+    {
+        static void Merge(int[] input, int l, int m, int r)
+        {
+            int i, j;
+            var n1 = m - l + 1;
+            var n2 = r - m;
+
+            var left = new int[n1];
+            var right = new int[n2];
+
+            for (i = 0; i < n1; i++)
+            {
+                left[i] = input[l + i];
+            }
+
+            for (j = 0; j < n2; j++)
+            {
+                right[j] = input[m + j + 1];
+            }
+
+            i = 0;
+            j = 0;
+            var k = l;
+
+            while (i < n1 && j < n2)
+            {
+                if (left[i] <= right[j])
+                {
+                    input[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    input[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                input[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                input[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+
+        static void SortMerge(int[] input, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                SortMerge(input, l, m);
+                SortMerge(input, m + 1, r);
+                Merge(input, l, m, r);
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortMerge(input, 0, input.Length - 1);
+            return input;
+        }
+    }
+}
---
+++ AlgorithmTests/Sorting/MergeSort/MergeSortTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.Sorting.MergeSort
+{
+    [TestClass]
+    public class MergeSortTests
+    {
+        [TestMethod]
+        public void MergeSortTestOne()
+        {
+            int[] expected = Algorithm.Sorting.MergeSort.MergeSort.Main(new[] { 1, 5, 2, 7, 8, 4 });
+            int[] actual = { 1, 2, 4, 5, 7, 8 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MergeSortTestTwo()
+        {
+            int[] expected = Algorithm.Sorting.MergeSort.MergeSort.Main(new[] { 100, 7, 290, 07, 18, 74 });
+            int[] actual = { 7, 07, 18, 74, 100, 290 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MergeSortTestThree()
+        {
+            int[] expected = Algorithm.Sorting.MergeSort.MergeSort.Main(new[] { -1, 52, -8964, 547, -191, 0, 85976 });
+            int[] actual = { -8964, -191, -1, 0, 52, 547, 85976 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
