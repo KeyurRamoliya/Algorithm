a6e771e4787b8639ac9e8ea49a3b63856e8f6f40
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-15 15:38:11 +0530

    Pigeonhole sort algorithm added

---
+++ Algorithm/Sorting/PigeonholeSort/PigeonholeSort.cs
@@ -0,0 +1,37 @@
+﻿namespace Algorithm.Sorting.PigeonholeSort
+{
+    public class PigeonholeSort
+    {
+        private static void SortPigeonhole(int[] input, int n)
+        {
+            int min = input[0], max = input[n];
+            for (int i = 1; i < n; i++)
+            {
+                if (input[i] < min) min = input[i];
+                if (input[i] > max) max = input[i];
+            }
+            int range = max - min + 1;
+            int[] holes = new int[range];
+
+            for (int i = 0; i < n; i++)
+            {
+                holes[input[i] - min] = input[i];
+            }
+            int index = 0;
+
+            for (int i = 0; i < range; i++)
+            {
+                foreach (var value in holes)
+                {
+                    input[index++] = value;
+                }
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortPigeonhole(input, input.Length);
+            return input;
+        }
+    }
+}
