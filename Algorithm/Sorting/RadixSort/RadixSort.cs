a563d5eeb682d3f753c22ed08715128c36d1e596
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-17 20:46:10 +0530

    Radix sort algorithm added

---
+++ Algorithm/Sorting/RadixSort/RadixSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sorting.RadixSort
+{
+    public class RadixSort
+    {
+        private static void SortRadix(int[] input, int n)
+        {
+            var temp = new int[n];
+
+            for (var shift = 31; shift > -1; --shift)
+            {
+                var j = 0;
+                for (var i = 0; i < n; ++i)
+                {
+                    var move = input[i] << shift >= 0;
+
+                    if (shift == 0 ? !move : move)
+                        input[i - j] = input[i];
+                    else
+                        temp[j++] = input[i];
+                }
+                Array.Copy(temp, 0, input, n - j, j);
+            }
+        }
+
+        public static int[] Main(int[] input)
+        {
+            SortRadix(input, input.Length);
+            return input;
+        }
+    }
+}
---
+++ AlgorithmTests/Sorting/RadixSort/RadixSortTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.Sorting.RadixSort
+{
+    [TestClass]
+    public class RadixSortTests
+    {
+        [TestMethod]
+        public void RadixSortTestOne()
+        {
+            int[] expected = Algorithm.Sorting.RadixSort.RadixSort.Main(new[] { 1, 5, 2, 7, 8, 4 });
+            int[] actual = { 1, 2, 4, 5, 7, 8 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RadixSortTestTwo()
+        {
+            int[] expected = Algorithm.Sorting.RadixSort.RadixSort.Main(new[] { 100, 7, 290, 07, 18, 74 });
+            int[] actual = { 7, 07, 18, 74, 100, 290 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RadixSortTestThree()
+        {
+            int[] expected = Algorithm.Sorting.RadixSort.RadixSort.Main(new[] { -1, 52, -8964, 547, -191, 0, 85976 });
+            int[] actual = { -8964, -191, -1, 0, 52, 547, 85976 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
