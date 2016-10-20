de365e8fd0174041b266f2ef3f64dda54b0ab321
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-20 19:26:45 +0530

    Integer Partition algorithm added

---
+++ Algorithm/DynamicProgramming/IntegerPartition/IntegerPartition.cs
@@ -0,0 +1,38 @@
+﻿namespace Algorithm.DynamicProgramming.IntegerPartition
+{
+    public class IntegerPartition
+    {
+        public static int[,] Result = new int[100,100];
+
+        private static int Partition(int targetNumber, int largestNumber)
+        {
+            for (int i = 1; i <= targetNumber; i++)
+            {
+                for (int j = 1; j <= largestNumber; j++)
+                {
+                    if (i - j < 0)
+                    {
+                        Result[i, j] = Result[i, j - 1];
+                        continue;
+                    }
+                    Result[i, j] = Result[i, j - 1] + Result[i - j, j];
+                }
+            }
+            return Result[targetNumber, largestNumber];
+        }
+
+        public static int Main(int number, int target)
+        {
+            int i;
+            for (i = 0; i <= number; i++)
+            {
+                Result[i, 0] = 0;
+            }
+            for (i = 1; i <= target; i++)
+            {
+                Result[0, i] = 1;
+            }
+            return Partition(number, target);
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/IntegerPartition/IntegerPartitionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.IntegerPartition
+{
+    [TestClass]
+    public class IntegerPartitionTests
+    {
+        [TestMethod]
+        public void IntegerPartitionTestOne()
+        {
+            int expected = Algorithm.DynamicProgramming.IntegerPartition.IntegerPartition.Main(8, 8);
+            int actual = 22;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IntegerPartitionTestTwo()
+        {
+            int expected = Algorithm.DynamicProgramming.IntegerPartition.IntegerPartition.Main(4, 4);
+            int actual = 5;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
