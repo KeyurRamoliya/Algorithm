e6cd747eb72ca349cb7c0b1c390df29817208e6a
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-21 19:37:02 +0530

    Knapsack Problem Algorithm Added

---
+++ Algorithm/DynamicProgramming/KnapsackProblem/KnapsackProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Algorithm.DynamicProgramming.KnapsackProblem
+{
+    public class KnapsackProblem
+    {
+        private static int Knapsack(int w, int[] weight, int[] value, int n)
+        {
+            int i;
+            int[,] k = new int[n + 1, w + 1];
+            for (i = 0; i <= n; i++)
+            {
+                int b;
+                for (b = 0; b <= w; b++)
+                {
+                    if (i==0 || b==0)
+                    {
+                        k[i, b] = 0;
+                    }
+                    else if (weight[i - 1] <= b)
+                    {
+                        k[i, b] = Math.Max(value[i - 1] + k[i - 1, b - weight[i - 1]], k[i - 1, b]);
+                    }
+                    else
+                    {
+                        k[i, b] = k[i - 1, b];
+                    }
+                }
+            }
+            return k[n, w];
+        }
+
+        public static int Main(int nItems, int[] weights, int[] values)
+        {
+            int n = values.Length;
+            return Knapsack(nItems, weights, values, n);
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/KnapsackProblem/KnapsackProblemTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.KnapsackProblem
+{
+    [TestClass]
+    public class KnapsackProblemTests
+    {
+        [TestMethod]
+        public void KnapsackProblemTestOne()
+        {
+            int expected = Algorithm.DynamicProgramming.KnapsackProblem.KnapsackProblem.Main(50, new[] {10, 20, 30},new[] {60, 100, 120});
+            int actual = 220;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void KnapsackProblemTestTwo()
+        {
+            int expected = Algorithm.DynamicProgramming.KnapsackProblem.KnapsackProblem.Main(10, new[] {5, 4, 6, 3},new[] {10, 40, 30, 50});
+            int actual = 90;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
