ce532df2a621ed1124e7d970cb808b3b504e7279
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-22 20:38:36 +0530

    Longest common subsequence algorithm added

---
+++ Algorithm/DynamicProgramming/LongestCommonSubsequence/LongestCommonSubsequence.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithm.DynamicProgramming.LongestCommonSubsequence
+{
+    public class LongestCommonSubsequence
+    {
+        private static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+        private static int Lcs(string x, string y, int m, int n)
+        {
+            var l = new int[m + 1, n + 1];
+            for (var i = 0; i <= m; i++)
+            {
+                for (var j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0) l[i,j] = 0;
+                    else if (x[i - 1] == y[j - 1]) l[i,j] = l[i - 1,j - 1] + 1;
+                    else l[i,j] = Max(l[i - 1,j], l[i,j - 1]);
+                }
+            }
+            return l[m, n];
+        }
+
+        public static int Main(string x, string y)
+        {
+            int m = x.Length;
+            int n = y.Length;
+            return Lcs(x, y, m, n);
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/LongestCommonSubsequence/LongestCommonSubsequenceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.LongestCommonSubsequence
+{
+    [TestClass]
+    public class LongestCommonSubsequenceTests
+    {
+        [TestMethod]
+        public void LongestCommonSubsequenceTestOne()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestCommonSubsequence.LongestCommonSubsequence.Main("AGGTAB", "GXTXAYB");
+            int actual = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LongestCommonSubsequenceTestTwo()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestCommonSubsequence.LongestCommonSubsequence.Main("ABCDEFG", "BCDGK");
+            int actual = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LongestCommonSubsequenceTestThree()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestCommonSubsequence.LongestCommonSubsequence.Main("ABCDGH", "AEDFHR");
+            int actual = 3;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
