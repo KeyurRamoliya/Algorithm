276aeb251ef2bee1a8446d58fe700ddaa295eeeb
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-23 20:47:52 +0530

    Longest Palindromic Subsequence Algorithm Added

---
+++ Algorithm/DynamicProgramming/LongestPalindromicSubsequence/LongestPalindromicSubsequence.cs
@@ -0,0 +1,38 @@
+﻿namespace Algorithm.DynamicProgramming.LongestPalindromicSubsequence
+{
+    public class LongestPalindromicSubsequence
+    {
+        private static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+        private static int Lps(string input, int n)
+        {
+            int[,] l = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                l[i, i] = 1;
+            }
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 0; j <  n - i + 1; j++)
+                {
+                    var k = j + i - 1;
+                    if (input[j] == input[k] && i == 2)
+                        l[j,k] = 2;
+                    else if (input[j] == input[k])
+                        l[j,k] = l[j + 1,k - 1] + 2;
+                    else
+                        l[j,k] = Max(l[j,k - 1], l[j + 1,k]);
+                }
+            }
+            return l[0, n - 1];
+        }
+
+        public static int Main(string input)
+        {
+            int n = input.Length;
+            return Lps(input, n);
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/LongestPalindromicSubsequence/LongestPalindromicSubsequenceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.LongestPalindromicSubsequence
+{
+    [TestClass]
+    public class LongestPalindromicSubsequenceTests
+    {
+        [TestMethod]
+        public void LongestPalindromicSubsequenceTestOne()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestPalindromicSubsequence.LongestPalindromicSubsequence.Main("THISISAPALINDROMEEMORDNILAPASISIHT");
+            int actual = 34;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LongestPalindromicSubsequenceTestTwo()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestPalindromicSubsequence.LongestPalindromicSubsequence.Main("AABCDEBAZ");
+            int actual = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LongestPalindromicSubsequenceTestThree()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestPalindromicSubsequence.LongestPalindromicSubsequence.Main("ACGTGTCAAAATCG");
+            int actual = 8;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
