2a39470d6a436415630ccc8ebff3ce0c6ecbcfbe
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-24 19:29:09 +0530

    Longest Increasing Subsequence algorithm added

---
+++ Algorithm/DynamicProgramming/LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithm.DynamicProgramming.LongestIncreasingSubsequence
+{
+    public class LongestIncreasingSubsequence
+    {
+        private static int Lis(int[] input, int n)
+        {
+            int[] lis = new int[n];
+            int max = 0;
+            for(int i = 0; i < n; i++)
+            {
+                lis[i] = 1;
+            }
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (input[i] > input[j] && lis[i] < lis[j] + 1)
+                        lis[i] = lis[j] + 1;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (max < lis[i])
+                    max = lis[i];
+            }
+            return max;
+        }
+
+        public static int Main(int[] input)
+        {
+            int n = input.Length;
+            return Lis(input, n);
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/LongestIncreasingSubsequence/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.LongestIncreasingSubsequence
+{
+    [TestClass]
+    public class LongestIncreasingSubsequenceTests
+    {
+        [TestMethod]
+        public void LongestIncreasingSubsequenceTestOne()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestIncreasingSubsequence.LongestIncreasingSubsequence.Main(new[] { 1, 5, 2, 7, 8, 4 });
+            int actual = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LongestIncreasingSubsequenceTestTwo()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestIncreasingSubsequence.LongestIncreasingSubsequence.Main(new[] { 10, 22, 9, 33, 21, 50, 41, 60 });
+            int actual = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LongestIncreasingSubsequenceTestThree()
+        {
+            int expected = Algorithm.DynamicProgramming.LongestIncreasingSubsequence.LongestIncreasingSubsequence.Main(new[] { 5, 2, 7, 4, 3, 8 });
+            int actual = 3;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
