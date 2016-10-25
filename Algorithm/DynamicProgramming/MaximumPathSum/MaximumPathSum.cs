bf155ad092e4cb98afc997fb3fb9258fb04f27cf
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-25 19:29:01 +0530

    Maximum Path Sum algorithm added

---
+++ Algorithm/DynamicProgramming/MaximumPathSum/MaximumPathSum.cs
@@ -0,0 +1,70 @@
+﻿//Reference : http://www.geeksforgeeks.org/find-maximum-path-sum-in-a-binary-tree/
+using System;
+
+namespace Algorithm.DynamicProgramming.MaximumPathSum
+{
+    public class Node
+    {
+
+        public int Value;
+        public Node Left, Right;
+
+        public Node(int item)
+        {
+            Value = item;
+            Left = Right = null;
+        }
+    }
+
+    class Res
+    {
+        public int Val;
+    }
+
+    public class MaximumPathSum
+    {
+        Node _root;
+        int FindMaxUtil(Node node, Res res)
+        {
+            if (node == null) return 0;
+            int l = FindMaxUtil(node.Left, res);
+            int r = FindMaxUtil(node.Right, res);
+            int maxSingle = Math.Max(Math.Max(l, r) + node.Value, node.Value);
+            int maxTop = Math.Max(maxSingle, l + r + node.Value);
+            res.Val = Math.Max(res.Val, maxTop);
+            return maxSingle;
+        }
+
+        int FindMaxSum()
+        {
+            return FindMaxSum(_root);
+        }
+
+        int FindMaxSum(Node node)
+        {
+            Res res = new Res {Val = Int32.MinValue};
+            FindMaxUtil(node, res);
+            return res.Val;
+        }
+
+        public static int Main()
+        {
+            MaximumPathSum tree = new MaximumPathSum
+            {
+                _root = new Node(10)
+                {
+                    Left = new Node(2),
+                    Right = new Node(10)
+                }
+            };
+            tree._root.Left.Left = new Node(20);
+            tree._root.Left.Right = new Node(1);
+            tree._root.Right.Right = new Node(-25)
+            {
+                Left = new Node(3),
+                Right = new Node(4)
+            };
+            return tree.FindMaxSum();
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/MaximumPathSum/MaximumPathSumTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.MaximumPathSum
+{
+    [TestClass]
+    public class MaximumPathSumTests
+    {
+        [TestMethod]
+        public void MaximumPathSumTestOne()
+        {
+            int expected = Algorithm.DynamicProgramming.MaximumPathSum.MaximumPathSum.Main();
+            int actual = 42;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
