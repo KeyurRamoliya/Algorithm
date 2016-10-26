6213b6e8565f66ca5f59f3ffa6fc65d9c772bc9e
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-26 21:00:23 +0530

    Maximum Subarray algorithm added

---
+++ Algorithm/DynamicProgramming/MaximumSubarray/MaximumSubarray.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithm.DynamicProgramming.MaximumSubarray
+{
+    public class MaximumSubarray
+    {
+        private static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        static int MaxSubArray(int[] input, int n)
+        {
+            int max = input[0];
+            int currentMax = input[0];
+            for (int i = 1; i < n; i++)
+            {
+                currentMax = Max(input[i], currentMax + input[i]);
+                max = Max(max, currentMax);
+            }
+            return max;
+        }
+
+        public static int Main(int[] input)
+        {
+            int n = input.Length;
+            return MaxSubArray(input, n);
+        }
+    }
+}
---
+++ AlgorithmTests/DynamicProgramming/MaximumSubarray/MaximumSubarrayTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests.DynamicProgramming.MaximumSubarray
+{
+    [TestClass]
+    public class MaximumSubarrayTests
+    {
+        [TestMethod]
+        public void MaximumSubarrayTestOne()
+        {
+            int expected = Algorithm.DynamicProgramming.MaximumSubarray.MaximumSubarray.Main(new[] { -2, -3, 4, -1, -2, 1, 5, -3 });
+            int actual = 7;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MaximumSubarrayTestTwo()
+        {
+            int expected = Algorithm.DynamicProgramming.MaximumSubarray.MaximumSubarray.Main(new[] { 1, 2, -3, -4, 2, 7, -2, 3 });
+            int actual = 10;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MaximumSubarrayTestThree()
+        {
+            int expected = Algorithm.DynamicProgramming.MaximumSubarray.MaximumSubarray.Main(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+            int actual = 6;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
