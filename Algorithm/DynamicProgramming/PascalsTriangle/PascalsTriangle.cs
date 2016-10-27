d4a9a81b3848d2c742e76f0c7981ebd2bdf947b0
KeyurRamoliya <keyurramoliya19@gmail.com>
2016-10-27 19:52:32 +0530

    Pascal's Triangel algorithm added

---
+++ Algorithm/DynamicProgramming/PascalsTriangle/PascalsTriangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithm.DynamicProgramming.PascalsTriangle
+{
+    public class PascalsTriangle
+    {
+        static void PascalTriangle(int n)
+        {
+            for (int line = 1; line <= n; line++)
+            {
+                int c = 1;
+                for (int i = 1; i <= line; i++)
+                {
+                    Console.WriteLine(c);
+                    c = c * (line - i) / i;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        public static int Main(int input)
+        {
+            PascalTriangle(input);
+            return input;
+        }
+    }
+}
